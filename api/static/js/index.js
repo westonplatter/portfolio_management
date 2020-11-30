7a9d349e5dacd7bdeaf7b1204d464db0fe8539d4
westonplatter <westonplatter@gmail.com>
2020-11-29 23:31:46 -0700

    favicon and reorganize js files for pages

--- api/static/js/index.js
+++ api/static/js/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import Main from "./Main";
+import App from "./App";
 import 'bootstrap';
 
 import "./bootstrap.scss";
 import "./index.scss";
  
-ReactDOM.render(<Main/>, document.getElementById("root"));
+ReactDOM.render(<App/>, document.getElementById("root"));
