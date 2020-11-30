994fa451c3e42a63643fd08cd7f07a14fbfc7bd5
westonplatter <westonplatter@gmail.com>
2020-11-30 00:38:22 -0700

    remove fake test

--- test/test_app.py
+++ test/test_app.py
@@ -9,6 +9,3 @@ class AppTest(AppTestCase):
     def test_invalid_path(self):
         res = self.client.get("/invalid")
         assert res.status_code == 404
-
-    def test_one_more(self):
-        assert 1 == 1
