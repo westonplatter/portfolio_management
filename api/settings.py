7619ef9e80a5024c1c68738a62a40dd888f64521
westonplatter <westonplatter@gmail.com>
2021-01-20 12:45:47 -0700

    try localhost since we're in the VM

--- api/settings.py
+++ api/settings.py
@@ -10,14 +10,6 @@ FLASK_ENV = app_env
 FLASK_APP = "server:app"
 DEBUG = 1 if app_env == "development" else 0
 
-port = environ.get("POSTGRES_PORT")
-
-print("--------------------")
-print(f"port = {port}")
-print("--------------------")
-
-uri = configs.get("SQLALCHEMY_DATABASE_URI")
-uri = uri.replace("5432", port)
-SQLALCHEMY_DATABASE_URI = uri
+SQLALCHEMY_DATABASE_URI = configs.get("SQLALCHEMY_DATABASE_URI")
 
 SQLALCHEMY_TRACK_MODIFICATIONS = configs.get("SQLALCHEMY_TRACK_MODIFICATIONS", 0)
