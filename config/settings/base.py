59087b82eac9333e4935a57b456f7a7dbfd1a961
westonplatter <westonplatter@gmail.com>
2021-03-22 23:34:30 -0600

    add some fields. start graphql

--- config/settings/base.py
+++ config/settings/base.py
@@ -83,6 +83,7 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     "portfolio_management.users.apps.UsersConfig",
     "ibkr",
+    "ingest",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
--- ibkr/models.py
+++ ibkr/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.fields import reverse_related
 
 
 class BaseModelMixin:
@@ -10,11 +11,31 @@ class Contract(models.Model, BaseModelMixin):
     con_id = models.IntegerField()
 
 
+char_field_defaults = dict(max_length=255, null=True)
+int_field_defaults = dict(null=True)
 class Trade(models.Model, BaseModelMixin):
-    account_id = models.IntegerField()
-    asset_category = models.CharField(max_length=255)
-    symbol = models.CharField(max_length=255)
-    description = models.CharField(max_length=255)
-    contract = models.ForeignKey('Contract', on_delete=models.CASCADE)
+    account_id = models.IntegerField(**int_field_defaults)
+    asset_category = models.CharField(**char_field_defaults)
+    symbol = models.CharField(**char_field_defaults)
+    description = models.CharField(**char_field_defaults)
+    contract = models.ForeignKey('Contract', on_delete=models.CASCADE, related_name='contract', null=True)
+    security_id = models.CharField(**char_field_defaults)
+    security_id_type = models.CharField(**char_field_defaults)
+    cusip = models.IntegerField(**int_field_defaults)
+    isin = models.CharField(**char_field_defaults)
+    listing_exchange = models.CharField(**char_field_defaults)
+    underlying_contract = models.ForeignKey('Contract', on_delete=models.CASCADE, related_name='underlying_contract', null=True)
+    underlying_symbol = models.CharField(null=True, max_length=20)
+    underlying_security_id = models.CharField(**char_field_defaults)
+    underlying_listing_exchange = models.CharField(**char_field_defaults)
+    issuer = models.CharField(**char_field_defaults)
+    multiplier = models.IntegerField(**int_field_defaults)
+    strike = models.DecimalField(null=True, blank=True, decimal_places=2, max_digits=10, default=None)
+    expiry = models.DateTimeField(null=True, blank=True, default=None)
+
+    # other fields
+
+    trade_id = models.IntegerField()
+    transaction_id = models.IntegerField(**int_field_defaults)
 
 
